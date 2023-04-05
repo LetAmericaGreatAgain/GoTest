23b10545fdf93ddce438d3dd0055760cd99dcca0
kanglitian <3315368931@qq.com>
2023-04-05 20:04:38 +0800

    新增了对go切片的使用示例

---
+++ slice.go
@@ -0,0 +1,58 @@
+package main
+
+import "fmt"
+
+func main(){
+	/**
+	 * 切片是对数组的抽象,是一种内置类型，相当于动态数组
+	 * 切片slice
+	 */
+	var slice1 []int //可以通过未指定数组大小的形式来声明一个切片
+
+    // slice1[0] = 999//报错
+    fmt.Printf("len=%d,capacity=%d\n",len(slice1),cap(slice1))
+
+    if(slice1 == nil){
+    	fmt.Println("slice1是一个空切片，还未进行初始化")
+    }
+	//也可以通过make函数来创建切片
+	var slice2 []int = make([]int,7,9) //7为切片的初始长度，9为切片的容量 
+    fmt.Printf("len=%d,capacity=%d\n",len(slice2),cap(slice2))
+
+	for i := 0;i<7;i++{//i范围到9的时候会执行异常
+		slice2[i] = i
+	}
+	for i:=0;i<7;i++{
+		fmt.Printf("slice2[%d]=%d\n",i,slice2[i])
+	}
+
+    //创建切片
+    numbers := []int{0,1,2,3,4,5,6,7,8,9}
+
+    //切片截取
+    fmt.Printf("len=%d,cap=%d,slice=%v\n",len(numbers),cap(numbers),numbers)
+    fmt.Println("numbers[1:4] == ",numbers[1:4])
+    fmt.Println("numbers[:4] == ",numbers[:4])//上限
+    fmt.Println("numbers[1:] == ",numbers[1:])//下限
+    
+    //append和copy函数，追加和复制切片
+    PrintSlice(slice1)
+
+    slice1 = append(slice1,0)//追加一个值
+    PrintSlice(slice1)
+    
+    slice1 = append(slice1,1,2,3)//追加多个值
+    PrintSlice(slice1)
+
+    slice3 := make([]int,len(slice1),cap(slice2)*2)
+    slice4 := make([]int,len(slice1),cap(slice2)*2)
+
+    copy(slice3,slice1)
+    PrintSlice(slice3)
+
+    copy(slice3,slice4)
+    PrintSlice(slice3)//len=4,cap=18,slice=[0 0 0 0]
+}
+func PrintSlice(arr []int){
+	fmt.Printf("len=%d,cap=%d,slice=%v\n",len(arr),cap(arr),arr)
+}
