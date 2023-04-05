92d80753de8e580c3e985c0f4e4dd08d5421ce75
kanglitian <3315368931@qq.com>
2023-04-05 21:56:57 +0800

    增加了map使用示例“

---
+++ map.go
@@ -0,0 +1,46 @@
+package main
+
+import "fmt"
+
+func main(){
+	var siteMap map[string]string
+
+	siteMap = make(map[string]string)
+
+	siteMap["google"] = "谷歌"
+	siteMap["apple"] = "苹果"
+	siteMap["unknown"] = "unknown"
+
+	for key,value := range siteMap{
+		fmt.Printf("key=%s,value=%s\n",key,value)
+	}
+
+	fmt.Println(siteMap["google"])
+
+	value,isExist := siteMap["YouTube"]
+	// if(isExist == nil){//(mismatched types bool and untyped nil)
+	// 	fmt.Println("value is not exist")
+	// }
+	if(isExist){
+		fmt.Printf("value exist and is %s\n",value)
+	}else{
+		fmt.Println("value is not exist")
+	}
+
+    delete(siteMap,"unknown")
+    fmt.Println("unknown 已经被删除")
+    for key,value := range siteMap{
+		fmt.Printf("key=%s,value=%s\n",key,value)
+	}
+
+	fmt.Printf("通过递归求1-n之和，和为：%d\n",SumByRecursion(100))
+}
+
+func SumByRecursion(n int)int{
+	if(n == 0){
+		return 0
+	}else{
+		return n+SumByRecursion(n-1)
+	}
+
+}
